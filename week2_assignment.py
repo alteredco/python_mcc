4224bdb54ff9ba76b64bf3dc48bb6a2c0e2cb6a9
Wendy <alteredco@gmail.com>
2018-08-21 15:58:38 +0200

    week 2 assignment

---
+++ week2_assignment.py
@@ -0,0 +1,14 @@
+# Create a greeting app. 
+# It should ask the user for their name and then greet them using the name.
+# eg. "Hello, (name)! How are you today?"
+
+
+# Create a ticket seller app.
+# It should ask the user's age and then sell a ticket with the following prices:
+# age 0 - 3 = free
+# age 3 - 9 or 65+ = $12
+# age 10 - 65 = $18
+# eg."You are (age). Your ticket will be (price) dollars, please"
+
+# Create an annoying backseat child app.
+# The child should ask, "Are we there yet?" until the user inputs "Yes"
